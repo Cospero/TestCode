c2c6cf3d26668830268eeb39414cb7a014b87a9d
Cospero <117189288+Cospero@users.noreply.github.com>
2024-01-19 17:48:15 +0300

    test2

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,13 +6,20 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _template;
     [SerializeField] private Transform[] _spawnPoints;
-
+    [SerializeField] private int NumToSpawn;
+    
     private void Start() 
     {
-        for(int i = 0; i<_spawnPoints.Length; i++ )     
+        for (int i=0; i < NumToSpawn; i++)  SpawnThings();
+            
+    }
+    
+    private void SpawnThings()
+    {
+        for(int i = 0; i < _spawnPoints.Length; i++ )     
         {
             Instantiate(_template,_spawnPoints[i].position, Quaternion.identity);
-        }      
+        }  
     }
 
    
